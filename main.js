a17263b3263164ea13b9a90a1d835418ed762e08
saddamarbaa <saddamarbaas@gmail.com>
2020-12-11 17:26:28 +0700

    Lecture #03 -  Window Methods - Alert, Prompt, Confirm

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* Javascript Dom Crash Course (JS BOM) */
 
-/*   Lecture -1 BOM Introduction
+/*   
+   Lecture #01 - BOM Introduction
    What IS BOM
    - Broweser Object Model
 
@@ -25,11 +26,11 @@
 
    - Colection Of Objects Control The Broswer
    - Default Object Of Browser IS Window
- 
 */
 
+
 /*   
-    Lecture -2  Window
+    Lecture #02 -  Window
     Window
     - Window Object Is The Browser Window
     - Window Object Suported By All Broswer
@@ -39,7 +40,7 @@
 
 // alert('Test')
 // window.alert('Test')
-
+window.console.log("ok")
 console.log(document.title)
 console.log(window.document.title)
 window.document.title = "new title"
@@ -50,3 +51,31 @@ console.log(document.getElementById('firstH2').textContent)
 console.log(window.document.getElementById('firstH2').textContent)
 
 // console.log(window.print())
+
+
+/*
+   Lecture #03 -  Window Methods - Alert, Prompt, Confirm
+*/
+
+// alert('hi\n nice \n to meet \n You')
+// Boolean Yes or Not
+// console.log(confirm("Are You Sure"))
+// console.log(confirm("Are You Sure"))
+// console.log(window.confirm("Are You Ok"))
+
+// let ask = window.confirm("Are You My Friend Ali?")
+// console.log(ask)
+
+// if (ask) {
+//     console.log("Yes that is ALI for sure")
+// } else {
+//     console.log("I know Your are not my friend")
+// }
+
+
+// let firstName = prompt("First Name :", "First Name : ")
+// let secondName = prompt("Second Name : ", "Second Name : ")
+// let fullName = firstName + " " + secondName
+// console.log("Your First Name is :  " + firstName)
+// console.log("Your Second Name is :  " + secondName)
+// console.log("Your Full Name is :  " + fullName)
