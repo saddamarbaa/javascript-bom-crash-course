f5109e1e53de27d718ddf73fae39c6b88615f1cc
saddamarbaa <saddamarbaas@gmail.com>
2020-12-11 16:23:46 +0700

    Lecture -1 BOM Introduction

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Javascript Dom Crash Course (JS BOM) */
+
+/*   Lecture -1 BOM Introduction
+   What IS BOM
+   - Broweser Object Model
+
+   What Can I Do With Dom
+   - Js Bom Talks To The Broswe
+   - Doing Action Not Related To Page
+   --- Change Url 
+   --- BackGround Request
+   --- Alert, Confirm, Prompt
+   --- Window Move, Window Open, Window Close 
+   --- Change Text In The State Bar
+
+   Bom Information
+   - ALL Modern Browsers Implement Similar Boms
+   --- Window
+   --- Screen
+   --- Location
+   --- History
+   --- Navigation
+   --- Timing
+   --- Cookies
+
+   - Colection Of Objects Control The Broswer
+   - Default Object Of Broser IS Window
+   - All Global Js Object Are Memeber Of Window Object
+*/
